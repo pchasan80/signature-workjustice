1bec2f84a17872d3d9874f86bf670816a59c3e85
Hasan Ali <alltimehasan2@gmail.com>
2025-12-17 18:27:07 +0600

    Phone Number Format Updated

--- script.js
+++ script.js
@@ -131,6 +131,15 @@ document.addEventListener('DOMContentLoaded', function() {
         previewBox.innerHTML = signatureHtml;
     }
 
+    // Phone formatting
+    const formatPhoneNumber = (e) => {
+        let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+        e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+    };
+
+    inputs.phone.addEventListener('input', formatPhoneNumber);
+    inputs.fax.addEventListener('input', formatPhoneNumber);
+
     // Event Listeners
     Object.values(inputs).forEach(input => {
         input.addEventListener('input', generateSignature);
